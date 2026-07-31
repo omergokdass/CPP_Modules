91ccb60fff4207436931c0a37a2e8f659fb7e4d3
ömer <faruk_gokdas@hotmail.com>
2026-07-31 17:58:45 +0300

    ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char** av)
+{
+    int i;
+
+    i = 1;
+    if(ac > 1)
+    {
+        while(av[i])
+        {
+            int j;
+            
+            j = 0;
+            while(av[i][j])
+            {
+                std::cout << (char)std::toupper(av[i][j]);
+                j++;
+            }
+            i++;
+        }
+        std::cout << std::endl;
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return 0;
+}
